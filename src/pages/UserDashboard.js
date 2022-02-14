895a50a32371afe4c4e54baa3228439ba9dbea3b
Shaun Limbeek <shaun.limbeek@gmail.com>
2022-02-14 09:11:22 -0800

    Create User dashboard, need to incorporate login services

---
+++ src/pages/UserDashboard.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import ProfileCard from "../components/ProfileCard";
+import ProfileForm from "../components/ProfileForm";
+import Header from '../components/Header';
+import AuthService from "../services/auth.service";
+import API from "../utils/API";
+
+const UserDashboard = function () {
+  const currentUser = AuthService.getCurrentUser();
+  const [displayForm, setDisplayForm] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  const getHomeId = () => {
+    const HomeId = currentUser.id;
+    return HomeId;
+  }
+
+  let HomeId = getHomeId();
+
+  useEffect(() => {
+    API.getUsers(HomeId)
+      .then(results => {
+        setUsers(results.data)
+      }).catch(err => console.error(err))
+  }, [])
+
+  const hideForm = () => {
+    setDisplayForm(false)
+  }
+  const showForm = () => {
+    setDisplayForm(true)
+  }
+
+  if (displayForm) {
+    return (
+        <>
+          <ProfileForm hideForm={hideForm} />
+        </>
+    )
+  } else {
+    return (
+        <>
+          <Header />
+          <ProfileCard showForm={showForm} users={users} />
+        </>
+    )
+  }
+};
+
+export default UserDashboard;
